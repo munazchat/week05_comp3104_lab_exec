e2f8623f4a275cd07f2a04dfee6bb672319d6761
munazchat <munazumrauf@gmail.com>
2020-10-07 21:43:01 -0400

    files added

---
+++ build/precache-manifest.51ff833b8a1fd900bb135b56623f25d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc3636eaad766f57bfa",
+    "url": "/static/css/main.995b5638.chunk.css"
+  },
+  {
+    "revision": "8cc3636eaad766f57bfa",
+    "url": "/static/js/main.8cc3636e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "1eb7c41c24e641b923260ebf413e654a",
+    "url": "/index.html"
+  }
+];
